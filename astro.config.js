f38814a9f3667bc626b90583bab060b820c30368
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2025-07-14 10:55:09 +0200

    feat: implement PDF creation (#108)

--- astro.config.js
+++ astro.config.js
@@ -1,3 +1,4 @@
+import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import { config } from '@dotenvx/dotenvx';
 import inline from '@playform/inline';
@@ -7,6 +8,7 @@ import unocss from 'unocss/astro';
 config();
 
 export default defineConfig({
+	adapter: netlify(),
 	base: '/',
 	i18n: {
 		defaultLocale: 'en',
@@ -27,5 +29,5 @@ export default defineConfig({
 		prefetchAll: true,
 	},
 	site: process.env.APPLICATION_SITE,
-	trailingSlash: 'always',
+	trailingSlash: 'never',
 });
---
+++ src/pages/[locale]/resume.pdf.ts
@@ -0,0 +1,54 @@
+import type { APIRoute } from 'astro';
+
+import { getDeployStore } from '@netlify/blobs';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ currentLocale, site }) => {
+	const url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/browser-rendering/pdf`;
+	const key = `resume-${currentLocale}.pdf`;
+
+	const store = getDeployStore('files');
+
+	let data = await store.get(key, { consistency: 'eventual', type: 'blob' });
+
+	if (!data) {
+		const options = getFetchOptions(`${site}/${currentLocale}`);
+		data = await fetch(url, options).then(async (response) => await response.blob());
+
+		await store.set(key, data);
+	}
+
+	return new Response(data, {
+		headers: {
+			'content-type': 'application/pdf',
+		},
+	});
+};
+
+const getFetchOptions = (url: string) => {
+	return {
+		body: JSON.stringify({
+			gotoOptions: {
+				waitUntil: 'load',
+			},
+			pdfOptions: {
+				format: 'a4',
+				margin: {
+					bottom: '15mm',
+					left: '15mm',
+					right: '15mm',
+					top: '15mm',
+				},
+				printBackground: true,
+			},
+			setJavaScriptEnabled: true,
+			url,
+		}),
+		headers: {
+			'authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+			'content-type': 'application/json',
+		},
+		method: 'POST',
+	};
+};
