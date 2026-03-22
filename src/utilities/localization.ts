f0198d9c1ab86e105504e17a675c5b0fd82976c9
Thomas Deinhamer <code@thasmo.dev>
2026-03-22 11:17:29 +0100

    chore: use type-safe current locale

--- src/utilities/localization.ts
+++ src/utilities/localization.ts
@@ -1,5 +1,11 @@
 import { defaultLocale, messages } from '../localization';
 
+export function getCurrentLocale(locale?: string): keyof typeof messages {
+	return (locale === 'en' || locale === 'de')
+		? (locale as keyof typeof messages)
+		: defaultLocale;
+}
+
 export function useDate(locale: keyof typeof messages) {
 	return function formatDate(value: string): string {
 		return new Date(value).toLocaleDateString(locale, { month: 'long', year: 'numeric' });
